ff85eb6a8c1118e5d25a5bcc13fc770c7def45b2
Francisco Campos <franciiscocampos170@gmail.com>
2024-06-21 11:57:29 +0100

    add listview

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
+
+const todos = [
+  { id: "1",
+    name:"buy fruits"
+  },
+  { id: "2",
+    name:"meeting with boss"
+  },
+  { id: "3",
+    name:"wash my car"
+  },
+  { id: "4",
+    name:"lunch with friends"
+  }
+]
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: '#fff' }}>
-        Open up App.js to start working on your app!
-      </Text>
+      <ScrollView>
+      {todos.map((todo) => {
+        return (
+          <View>
+            <Text style={styles.item}>{todo.name}</Text>
+          </View>
+        );
+      })}
       <StatusBar style="auto" />
+      </ScrollView>
     </View>
   );
 }
@@ -15,8 +36,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+    backgroundColor: '#f1f2f6',
+    //alignItems: 'center',
     justifyContent: 'center',
   },
+  item:{
+    padding:20,
+    fontSize:15,
+    marginTop:5
+  }
 });
